696c5f507f6de7161c4e746bc8c506704e175b30
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-07 23:04:18 +0300

    import them to the correct directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import logo_light from "./assets/logo-black.png";
-import logo_dark from "./assets/logo-white.png";
-import search_icon_light from "./assets/search-w.png";
-import search_icon_dark from "./assets/search-b.png";
-import toggle_light from "./assets/night.png";
-import toggle_dark from "./assets/day.png";
-
 
 function App() {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import logo_light from '../../assets/logo-black.png'
+import logo_dark from "../../assets/logo-white.png";
+import search_icon_light from "../../assets/search-w.png";
+import search_icon_dark from "../../assets/search-b.png";
+import toggle_light from "../../assets/night.png";
+import toggle_dark from "../../assets/day.png";
+
 
 function Navbar() {
   return (
