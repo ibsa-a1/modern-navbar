d8a654abba16328123d1811a064d5895784c118b
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-07 23:38:31 +0300

    initialize theme as light

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import react, { useState } from "react";
 
 function App() {
+
+  const [theme, seTheme] = useState('light')
+
   return (
     <div className="container">
       <Navbar />
