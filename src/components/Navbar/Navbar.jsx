0076f45227aff153583dd79816e49e0bc4890937
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-07 23:54:06 +0300

    basics for dark theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,9 +9,14 @@ import toggle_dark from "../../assets/day.png";
 
 
 function Navbar({theme, setTheme}) {
+
+    const toggle_mode = () => {
+      theme == 'light' ? setTheme('dark') : setTheme('light')
+    };
+
   return (
     <div className='navbar'>
-      <img src={logo_light} alt='' className='logo' />
+      <img src={theme == 'light' ? logo_light : logo_dark} alt='' className='logo' />
       <ul>
         <li>Home</li>
         <li>Products</li>
@@ -21,10 +26,10 @@ function Navbar({theme, setTheme}) {
 
       <div className="search-box">
         <input type="text" placeholder='Search' />
-        <img src={search_icon_light} alt=''/>
+        <img src={theme == 'light' ? search_icon_light : search_icon_dark} alt=''/>
       </div>
 
-      <img src={toggle_light} alt='' className='toggle-icon'/>
+      <img onClick={()=>toggle_mode()} src={theme == 'light' ? toggle_light : toggle_dark} alt='' className='toggle-icon'/>
     </div>
   )
 }
