cadf275788911deba5747566fade57c8fcbd54ea
ibsa-a1 <ibsaabera712@gmail.com>
2025-07-09 23:03:17 +0300

    setup our menu icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,13 +6,18 @@ import search_icon_light from "../../assets/search-w.png";
 import search_icon_dark from "../../assets/search-b.png";
 import toggle_light from "../../assets/night.png";
 import toggle_dark from "../../assets/day.png";
-import menu_light from '../../assets/menu-light.jpg'; 
-import menu_dark from '../../assets/menu-dark.jpg';   
-import close_light from '../../assets/close-light.jpg'; 
-import close_dark from '../../assets/close-dark.jpg';   
+import menu_light from '../../assets/menu_light.jpg'; 
+import menu_dark from '../../assets/menu_dark.jpg';   
+import close_light from '../../assets/close_light.jpg'; 
+import close_dark from '../../assets/close_dark.jpg';   
 
 
 function Navbar({theme, setTheme}) {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+      setMobileMenuOpen(!mobileMenuOpen);
+    };
 
     const toggle_mode = () => {
       theme == 'light' ? setTheme('dark') : setTheme('light')
@@ -21,6 +26,18 @@ function Navbar({theme, setTheme}) {
   return (
     <div className='navbar'>
       <img src={theme == 'light' ? logo_light : logo_dark} alt='' className='logo' />
+
+      <img
+        src={
+          mobileMenuOpen
+            ? (theme === 'light' ? close_light : close_dark) // If menu is open
+            : (theme === 'light' ? menu_light : menu_dark)   // If menu is closed
+        }
+        alt=''
+        className='menu-icon'
+        onClick={toggleMobileMenu}
+      />
+
       <ul>
         <li>Home</li>
         <li>Products</li>
